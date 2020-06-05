cc828d0e9810e614cede228e2cd67a1fa33ed8c5
Marcin Olek <marcin@spotio.com>
2020-06-05 17:35:22 +0200

    stylunie

--- src/calendar/header/style.js
+++ src/calendar/header/style.js
@@ -1,17 +1,17 @@
-import { StyleSheet, Platform } from 'react-native';
-import * as defaultStyle from '../../style';
+import { StyleSheet, Platform } from "react-native";
+import * as defaultStyle from "../../style";
 
-const STYLESHEET_ID = 'stylesheet.calendar.header';
+const STYLESHEET_ID = "stylesheet.calendar.header";
 
-export default function(theme = {}) {
+export default function (theme = {}) {
   const appStyle = { ...defaultStyle, ...theme };
   return StyleSheet.create({
     header: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
+      flexDirection: "row",
+      justifyContent: "space-between",
       paddingLeft: 10,
       paddingRight: 10,
-      alignItems: 'center'
+      alignItems: "center",
     },
     monthText: {
       fontSize: appStyle.textMonthFontSize,
@@ -19,36 +19,36 @@ export default function(theme = {}) {
       fontWeight: appStyle.textMonthFontWeight,
       color: appStyle.monthTextColor,
       marginTop: 0,
-      marginBottom: 2
+      marginBottom: 2,
     },
     arrow: {
-      padding: 10
+      padding: 10,
     },
     arrowImage: {
       ...Platform.select({
         ios: {
-          tintColor: appStyle.arrowColor
+          tintColor: appStyle.arrowColor,
         },
         android: {
-          tintColor: appStyle.arrowColor
-        }
-      })
+          tintColor: appStyle.arrowColor,
+        },
+      }),
     },
     week: {
       marginTop: 3,
-      flexDirection: 'row',
-      justifyContent: 'space-around'
+      flexDirection: "row",
+      justifyContent: "space-around",
     },
     dayHeader: {
-      marginTop: 21,
+      marginTop: 4,
       marginBottom: -4,
       width: 32,
-      textAlign: 'center',
+      textAlign: "center",
       fontSize: appStyle.textDayHeaderFontSize,
       fontFamily: appStyle.textDayHeaderFontFamily,
       fontWeight: appStyle.textDayHeaderFontWeight,
-      color: appStyle.textSectionTitleColor
+      color: appStyle.textSectionTitleColor,
     },
-    ...(theme[STYLESHEET_ID] || {})
+    ...(theme[STYLESHEET_ID] || {}),
   });
 }
